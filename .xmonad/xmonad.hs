01f7a8ebf761628d16704cce9d1d420544293435
playahater <mishkez@gmail.com>
2016-11-24 11:56:57 +0100

    hdpi settings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -142,7 +142,7 @@ myFocusedBorderColor = "#400000"
 
 -- some nice colors for the prompt windows
 myXPConfig = defaultXPConfig {
-      font = "xft:Literation Mono Powerline:pixelsize=14:antialias=true:autohint=true:hinting=true:dpi=96"
+      font = "xft:Literation Mono Powerline:pixelsize=16:antialias=true:autohint=true:hinting=true:dpi=144"
     , bgColor = "#282828"
     , fgColor = "#D7D0C7"
     , fgHLight = "#D7D0C7"
